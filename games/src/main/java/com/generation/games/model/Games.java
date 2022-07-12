a96cb6521289af472a0943b27ec98c1751e989db
Gustavo <jgustavocambui@gmail.com>
2022-07-12 14:33:28 -0300

    Adicionando atividade security Back-end loja de games

--- games/src/main/java/com/generation/games/model/Games.java
+++ games/src/main/java/com/generation/games/model/Games.java
@@ -43,6 +43,16 @@ public class Games {
 	@JsonIgnoreProperties("games")
 	private Categoria categoria;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("games")
+	private Usuario usuario;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	public Categoria getCategoria() {
 		return categoria;
 	}
